11b20ca05bf4d64d8acc96fef3c66b792b7934a0
Alex Clifford <aclifford336@gmail.com>
2020-05-01 16:49:02 +0100

    one-to-many: forms - bonus

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,6 +6,19 @@ class ReviewsController < ApplicationController
     @reviews = @movie.reviews
   end
 
+  def edit
+    @review = @movie.reviews.find(params[:id])
+  end
+
+  def update
+    @review = @movie.reviews.find(params[:id])
+    if @review.update(review_params)
+      redirect_to movie_reviews_url(@movie), notice: "Review successfully updated!"
+    else
+      render :edit
+    end
+  end
+
   def new
     @review = @movie.reviews.new
   end
@@ -17,6 +30,12 @@ class ReviewsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @review = @movie.reviews.find(params[:id])
+    @review.destroy
+    redirect_to movie_reviews_url(@movie), alert: "Review successfully deleted!"
   end
 
   private
