2f598403220082157924bd4ebca67f14dd670a77
Alex Clifford <aclifford336@gmail.com>
2020-05-04 16:56:58 +0100

    edit / delete user account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "Account successfully updated!"
+    else
+      render :edit
+    end
+  end
+
   def new
     @user = User.new
   end
@@ -21,11 +34,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.delete
+    redirect_to movies_url, alert: "Account successfully deleted!"
+  end
+
   private
 
   def user_params
     params.require(:user)
-      .permit(:name, :email, :password, :password_confirmation)
+      .permit(:name, :username, :email, :password, :password_confirmation)
   end
 
 end
