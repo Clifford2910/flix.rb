3aeaaf76ad9b411455606d78ccbff6dfdf12e0a6
Alex Clifford <aclifford336@gmail.com>
2020-05-05 12:13:50 +0100

    sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   root "movies#index"
+
   resources :movies do
     resources :reviews
   end
+
+  resource :session, only: [:new, :create, :destroy]
+  get "signin" => "sessions#new"
+
   resources :users
   get "signup" => "users#new"
+
 end
