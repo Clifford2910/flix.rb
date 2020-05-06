1e4cef85a90352d928cadc6e9a1c8626de22a16d
Alex Clifford <aclifford336@gmail.com>
2020-05-06 16:40:35 +0100

    authorisation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -9,11 +11,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Account successfully updated!"
     else
@@ -36,13 +36,17 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.delete
     session[:user_id] = nil
     redirect_to movies_url, alert: "Account successfully deleted!"
   end
 
   private
+
+  def require_correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url unless current_user?(@user)
+  end
 
   def user_params
     params.require(:user)
