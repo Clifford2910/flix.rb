cbe0cf67f847268b2b4a0b0580ef63895c429db6
Alex Clifford <aclifford336@gmail.com>
2020-05-07 15:53:26 +0100

    many-to-many: associations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :require_signin
   before_action :set_movie
 
   def index
@@ -24,6 +25,7 @@ class ReviewsController < ApplicationController
 
   def create
     @review = @movie.reviews.new(review_params)
+    @review.user = current_user
     if @review.save
       redirect_to movie_reviews_url(@movie), notice: "Thanks for your review!"
     else
@@ -41,7 +43,7 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review)
-      .permit(:name, :stars, :comment)
+      .permit(:stars, :comment)
   end
 
   def set_movie
